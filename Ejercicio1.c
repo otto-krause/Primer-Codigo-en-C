60ad1914d54989dbfd8856932e6069698267d61d
gusgatam <gustavo.gatamora@gmail.com>
2021-06-23 11:34:04 -0300

    Cambios
    Se agregó color a la consola.

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -4,10 +4,12 @@
 void main ()
 {
     int a=0, b=0;
-    printf ("Ingrese el primer valor ");
+    printf ("Ingrese el primer valor ");//Debo respetar los espacios entre las comillas
     scanf ("%d",&a);
     printf("Ingrese el segundo valor ");
     scanf("%d", &b);
-    system ("cls");
+    /*system ("cls");
+utilizo esto para cometar mas de una linea*/
+    system("color 3A");
     printf("Los valores ingresados fueron: \n%d\n%d", a, b);
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Primer Codigo en C
- C
+ 0 = Negro 
+1 = Azul 
+2 = Verde 
+3 = Aguamarina 
+4 = Rojo 
+5 = Purpura 
+6 = Amarillo 
+7 = Blanco 
+8 = Gris 
+9 = Azul claro 
+A = Verde claro 
+B = Aguamarina claro
+C = Rojo claro
+D = Purpura claro
+E = Amarillo claro
+F = Blanco brillante
